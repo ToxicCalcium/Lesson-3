debd91d24b6822e1c8868f3328ca2c5c83e2bf0b
ToxicCalcium <166393308+ToxicCalcium@users.noreply.github.com>
2024-06-22 17:02:21 +0100

    Add files via upload

---
+++ Lesson 3 - shoot the alien/AlienPewPew.py
@@ -0,0 +1,18 @@
+import pgzrun
+import random
+
+#window title
+TITLE = "Alien Pew Pew"
+WIDTH = 500
+HEIGHT =500
+
+message = ""
+
+#Character Creation
+special_Pew_Pew_Person = Actor("download")
+special_Pew_Pew_Person.pos = (250, 250)
+
+def draw():
+
+
+pgzrun.go()
